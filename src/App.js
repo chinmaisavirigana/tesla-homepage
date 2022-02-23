851a23e170df38e60820620136b920bef9c4e399
Savirigana Srikanth (TECH MAHINDRA LTD.) <v-sasrikanth@microsoft.com>
2022-02-23 17:49:40 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import Header from './components/Header';
+import Home from './components/Home';
+//import Section from './components/Section';
+
+function App() {
+  return (
+    <div className="App">
+      <Header/>
+      <Home/>
+      {/* <Section/> */}
+    </div>
+  );
+}
+
+export default App;
